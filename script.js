15af28380f7aca3218f6b3d0d3e10a1777232641
Dazaii23 <155020227+Dazaii23@users.noreply.github.com>
2025-08-06 22:35:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 
-    // 1. Dodavanje kontrolne strukture (if ili if-else)
+   <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0F460T1KRW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0F460T1KRW');
+</script>
     let x = 5;
     if (x > 3) {
         console.log("Broj je veći od 3.");
     }
 
-    // 2. Dodavanje petlje (for, while ili do-while)
+    
     for (let i = 0; i < 5; i++) {
         console.log("Vrednost i je: " + i);
     }
 
-    // 3. Kreiranje niza
+    
     let niz = [1, 2, 3, 4, 5];
 
-    // 4. Implementacija minimum dve funkcije sa ulaznim parametrima i povratnim vrednostima
+
     function saberi(a, b) {
         return a + b;
     }
@@ -22,7 +30,7 @@
         return a * b;
     }
 
-    // 5. Dodavanje minimum dva događaja
+    
     document.getElementById("myButton").addEventListener("click", function() {
         console.log("Dugme je kliknuto.");
     });
@@ -31,13 +39,17 @@
         console.log("Vrednost input polja je promenjena.");
     });
 
-    // 6. Promena stila elemenata
+  
     document.getElementById("myElement").style.color = "red";
 
-    // 7. Preuzimanje podataka iz forme
+    
     document.getElementById("myForm").addEventListener("submit", function(event) {
         event.preventDefault();
         let name = document.getElementById("name").value;
         let email = document.getElementById("email").value;
         console.log("Ime: " + name + ", Email: " + email);
+        <!-- Google tag (gtag.js) -->
+
+</script>
+
     });
